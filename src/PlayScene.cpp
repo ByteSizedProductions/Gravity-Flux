192c1c7a7b4953a601298c79091e2f0a03a2b397
Unknown <seanjasminsnelson@gmail.com>
2021-04-09 21:25:02 -0400

    Took some crates away in Level 2 and fixed door bug

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -61,6 +61,20 @@ void PlayScene::update()
 	}
 
 	m_AbilityBar->setAbilityCooldown(double(m_pMarvin->getGravityCooldown()));
+
+	if (CollisionManager::AABBCheck(m_pMarvin, m_pDoor))
+	{
+		if (m_level == 3)
+		{
+			m_level = 1;
+			TheGame::Instance()->changeSceneState(START_SCENE);
+		}
+		else
+		{
+			m_level++;
+			TheGame::Instance()->changeSceneState(LOADING_SCENE);
+		}
+	}
 }
 
 void PlayScene::updateCollisions()
@@ -126,7 +140,7 @@ void PlayScene::updateCollisions()
 			}
 		}
 	}
-	if (CollisionManager::AABBCheck(m_pMarvin, m_pDoor))
+	/*if (CollisionManager::AABBCheck(m_pMarvin, m_pDoor))
 	{
 		if (m_level == 3)
 		{
@@ -138,7 +152,7 @@ void PlayScene::updateCollisions()
 			m_level++;
 			TheGame::Instance()->changeSceneState(LOADING_SCENE);
 		}
-	}
+	}*/
 	if (m_level == 3)
 	{
 		if (CollisionManager::AABBCheck(m_pMarvin, m_pBossEnemy))
