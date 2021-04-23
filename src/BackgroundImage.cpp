b0d36152de74573108beace78ff99643be5f3134
ReubenCserepka <reuben.cserepka@gmail.com>
2021-04-23 18:42:46 -0400

    Background fixed

--- src/BackgroundImage.cpp
+++ src/BackgroundImage.cpp
@@ -8,7 +8,7 @@ BackgroundImage::BackgroundImage()
 	auto size = TextureManager::Instance()->getTextureSize("background");
 	setWidth(size.x);
 	setHeight(size.y);
-	getTransform()->position = glm::vec2(-350.0f, -250.0f);
+	getTransform()->position = glm::vec2(0.0f, 0.0f);
 	setType(BACKGROUND);
 	getRigidBody()->isColliding = false;
 }
@@ -22,8 +22,69 @@ void BackgroundImage::draw()
 	const auto x = getTransform()->position.x;
 	const auto y = getTransform()->position.y;
 
-	TextureManager::Instance()->draw("background", getTransform()->position.x, getTransform()->position.y, 0, 255, false);
-	//TextureManager::Instance()->drawFromSheet("background", 0, 0, 3200, 2400, 3200, 2000, x, y, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y - 320.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x - 320.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 480.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 1280.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2080.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 2880.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 3680.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
+
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 320.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 280.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 880.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 1480.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 2080.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 2680.0f, 0, 255, false);
+	TextureManager::Instance()->draw("background", getTransform()->position.x + 4480.0f, getTransform()->position.y + 3280.0f, 0, 255, false);
 }
 
 void BackgroundImage::update()
